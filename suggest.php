e3f82c30771be18023058db15a68304e08815a2b
Jerry Relunia <jrelunia@team.com>
2016-03-03 08:24:29 -0500

    add forms

--- suggest.php
+++ suggest.php
@@ -1,13 +1,39 @@
-<?php 
+<?php
 $pageTitle = "Suggest a media item";
 $section = "suggest";
 
-include("inc/header.php"); ?>
-
-
+include("inc/header.php");
+?>
+	
 <div class="section page">
-	<h1><?php echo $pageTitle ?></h1>
+	<div class="wrapper">
+		<h1>Suggest a media item</h1>
+		<p>If you think there is something missing, let me know! Complete form to send email.</p>
+		<form method="post" action="process.php">
+			<table>
+				<tr>
+					<th><label for="name">Name </label></th>
+					<td><input type="text" id="name" name="name"></td>
+					
+				</tr>
+				<tr>
+					<th><label for="email">Email </label></th>
+					<td><input type="text" id="email" name="email"></td>
+					
+				</tr>
+				<tr>
+					<th><label for="details">Suggest Item details </label></th>
+					<td><textarea name="details" id="details"></textarea></td>
+				</tr>
+				
+			</table>
+			
+			<input type="submit" value="send">
+			
+		</form>
+	</div>
 </div>
 
-
-<?php include("inc/footer.php"); ?>
+<?php 
+include("inc/footer.php");	
+?>
---
+++ thanks.php
@@ -0,0 +1,17 @@
+<?php
+
+$pageTitle = "Thank you";
+$section = null;
+include("inc/header.php");
+?>
+
+
+<div class="section page">
+	<h1>Thank you</h1>
+	<p>Thanks for your email! I&rsquo;ll check out your suggestion shortly!</p>
+</div>
+
+
+
+<?php include("inc/footer.php"); 
+?>
