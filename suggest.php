dd1462ffe847f7dcb8d42a9cf84f38b8ebc86f0a
Jerry Relunia <jrelunia@team.com>
2016-03-03 09:51:32 -0500

    more validation done on suggest form

--- suggest.php
+++ suggest.php
@@ -1,4 +1,35 @@
-<?php
+<?php 
+if  ($_SERVER["REQUEST_METHOD"] == "POST") {
+ //trim whitespace before and after before saving post as variable
+//use filter_input (type, name, type of filter)
+	
+    $name = trim(filter_input(INPUT_POST,"name", FILTER_SANITIZE_STRING));
+    $email = trim(filter_input(INPUT_POST,"email", FILTER_SANITIZE_EMAIL));
+    $details = trim(filter_input(INPUT_POST,"details", FILTER_SANITIZE_SPECIAL_CHARS));
+
+//validation of forms
+    if ($name == "" || $email == "" || $details = "") {
+	    echo 'Please fill in the required names: Name, email, and details';
+	    exit;
+    }
+	
+    //honeypot for bots spam
+    if ($_POST["address"] !== "") {
+        echo "bad form input";
+        exit;
+	}
+	echo "<pre>";
+	$email_body = "";
+	$email_body .= "Name " . $name . "\n";
+	$email_body .= "Email " . $email . "\n";
+	$email_body .= "Details " . $details . "\n";
+	echo $email_body;
+	echo "</pre>";
+
+	//To do : send email
+	header("location:thanks.php");
+}
+   
 $pageTitle = "Suggest a media item";
 $section = "suggest";
 
@@ -9,7 +40,7 @@ include("inc/header.php");
 	<div class="wrapper">
 		<h1>Suggest a media item</h1>
 		<p>If you think there is something missing, let me know! Complete form to send email.</p>
-		<form method="post" action="process.php">
+		<form method="post" action="suggest.php"> <!-- post the form back to own page -->
 			<table>
 				<tr>
 					<th><label for="name">Name </label></th>
@@ -24,6 +55,12 @@ include("inc/header.php");
 				<tr>
 					<th><label for="details">Suggest Item details </label></th>
 					<td><textarea name="details" id="details"></textarea></td>
+				</tr>
+<!--				honeypot for bots spam, won't be shown in browser-->
+				<tr style="display:none">
+					<th><label for="address">Address </label></th>
+					<td><input type="text" name="details" id="address"></input></td>
+					<p>Please leave this field blank</p>
 				</tr>
 				
 			</table>
